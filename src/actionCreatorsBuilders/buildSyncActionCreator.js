447676c6276afa69efcfb6ab5c4f17287bbf5af7
kazagkazag <kamil@zagrabski.com>
2017-05-01 11:28:15 +0200

    buildSyncActionCreator modified to conform FSA standard.

--- src/actionCreatorsBuilders/buildSyncActionCreator.js
+++ src/actionCreatorsBuilders/buildSyncActionCreator.js
@@ -3,10 +3,13 @@
 // it is simple function taken from: http://redux.js.org/docs/recipes/ReducingBoilerplate.html
 export default function buildSyncActionCreator(type: string, ...argNames: Array<any>): Function {
     return (...args: Array<any>): Object => {
-        const action = { type };
+        const action = {
+            type,
+            payload: {}
+        };
 
         argNames.forEach((arg, index) => {
-            action[argNames[index]] = args[index];
+            action.payload[argNames[index]] = args[index];
         });
 
         return action;
--- src/actionCreatorsBuilders/buildSyncActionCreator.test.js
+++ src/actionCreatorsBuilders/buildSyncActionCreator.test.js
@@ -1,15 +1,20 @@
+import { isFSA } from "flux-standard-action";
 import buildSyncActionCreator from "./buildSyncActionCreator";
 
-test("should create synchronous action creator that creates action", () => {
+test("should create synchronous action creator that creates FSA action", () => {
     const type = "my/type";
     const expectedAction = {
         type,
-        fieldA: "property A",
-        fieldB: "property B"
+        payload: {
+            fieldA: "property A",
+            fieldB: "property B"
+        }
     };
     const actionCreator = buildSyncActionCreator(type, "fieldA", "fieldB");
-    const action = actionCreator(expectedAction.fieldA, expectedAction.fieldB);
+    const action = actionCreator(expectedAction.payload.fieldA, expectedAction.payload.fieldB);
+    const conformsFSA = isFSA(action);
 
     expect(typeof actionCreator).toBe("function");
     expect(action).toEqual(expectedAction);
+    expect(conformsFSA).toBeTruthy();
 });
