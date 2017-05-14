2b48b5560ab6984ba31553b2a2b3768161147eaf
kazagkazag <kamil@zagrabski.com>
2017-05-14 07:59:14 +0200

    Added new features to the index.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import buildRequestActionCreator from "./actionCreatorsBuilders/request";
+import request from "./actionCreatorsBuilders/request";
 import buildSyncActionCreator from "./actionCreatorsBuilders/buildSyncActionCreator";
+import init from "./core/init";
+import buildReducers from "./reducersBuilders/buildReducers";
 
 export default {
-    buildRequestActionCreator,
-    buildSyncActionCreator
+    buildSyncActionCreator,
+    request,
+    init,
+    buildReducers
 };
