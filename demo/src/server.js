e1eeda5be3e639608d6ba31dc781da219f8cf46e
kazagkazag <kamil@zagrabski.com>
2017-05-18 19:06:39 +0200

    Added rest mock to the demo

---
+++ demo/src/server.js
@@ -0,0 +1,48 @@
+const restify = require("restify");
+
+function getUser(req, res, next) {
+    res.send({
+        name: "John",
+        age: "24",
+        role: "author"
+    });
+    next();
+}
+
+function getPosts(req, res, next) {
+    if (Math.random() > 0.5) {
+        res.send({
+            posts: [
+                {
+                    id: 1,
+                    title: "First post"
+                },
+                {
+                    id: 2,
+                    title: "Second post"
+                }
+            ]
+        });
+    } else {
+        res.send(400, {
+            reasons: [
+                "Error while fetching data from DB.",
+                "DB connection aborted."
+            ]
+        });
+    }
+    next();
+}
+
+function logoutUser(req, res, next) {
+    res.send(200);
+    next();
+}
+
+const server = restify.createServer();
+
+server.get("api/user", getUser);
+server.get("api/posts", getPosts);
+server.post("api/user/logout", logoutUser);
+
+server.listen(8080);
